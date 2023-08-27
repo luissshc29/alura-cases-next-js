d644c12ae0ad903620d811cd92a145d33a6aff86
Luis Henrique <luishcc2003@gmail.com>
2023-08-27 19:37:08 -0300

    Alterando _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,6 @@ export default function App({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet" />
             </Head>
             <GlobalStyle/>
             <Component {...pageProps} />
